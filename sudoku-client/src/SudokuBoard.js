63f5c04307a07c51e740aee0a6578cb81b92b627
Andy Wilson <atwilso@sandia.gov>
2020-04-07 00:44:20 -0600

    Board now prints block coordinates
    Wow, this took more work than it seemed like it should.
    
    SudokuBoard now prints a DxD array of tuples.  Each tuple is the
    coordinates (row, column) of that particular block.
    
    Now that I know how to do this it should be a straight shot to
    generating the blocks within each cell.

--- sudoku-client/src/SudokuBoard.js
+++ sudoku-client/src/SudokuBoard.js
@@ -14,6 +14,7 @@
 // value.  
 
 import React from 'react';
+// import { range } from './SudokuUtilities';
 
 class SudokuBoard extends React.Component {
 	constructor(props) {
@@ -23,13 +24,56 @@ class SudokuBoard extends React.Component {
 		};
 	}
 
+	/// Make the array of squares, blocks, and whatnot.
+	//
+	// We can't have loops inside the return() clause in render(). 
+	// Instead, what we do is set up a dummy element in return()
+	// and inside that dummy element put the result of a function
+	// call.  
+	makeBoardComponents(moveLists) {
+		const D = this.props.degree;
+		var r, c;
+		var thisRow;
+	
+		// This is the top-level container for everything.
+		var table = [];
+		var children = [];
+
+		for (r = 0; r < D; ++r) {
+			thisRow = [];
+			for (c = 0; c < D; ++c) {
+				thisRow.push(<span key={D*r + c}>({r},{c})</span>);
+			}
+			children.push(<div key={r}>{thisRow}</div>);
+		}
+
+		table.push(children);
+		return table;
+	}
+
+
 	render() {
 		return (
-			<div>
-				This is a Sudoku board.  Its degree is {this.props.degree}.
+			<div key="10" className="entireBoardPlusDecorations">
+				<div key="1">
+					This is a Sudoku board.  Its degree is {this.props.degree}.
+					This object's responsibilities are as follows:
+					<ul>
+						<li>Receive the board state from the game tree</li>
+						<li>Set up a D^2 x D^2 array of SudokuSquare objects</li>
+						<li>Pass the relevant "permitted moves" list to each square</li>
+						<li>Identify which square has been clicked</li>
+					</ul>
+				</div>
+				<p key="2">The next element down is the number board.</p>
+				<div key="3" id="board-{this.state.serialNumber}">
+					{this.makeBoardComponents({})}
+				</div>
 			</div>
 		);
 	}
+
+
 }
 
 
