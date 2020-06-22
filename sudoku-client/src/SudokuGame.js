fb3bde79e174bf97716c3951ac8e2ec6119d22b9
Andy Wilson <atwilso@sandia.gov>
2020-06-22 15:44:10 -0600

    SudokuGame now tracks game tree
    We're now rendering from the game tree instead of rendering a board
    directly.  It's still hardcoded to only have a single root node...
    but it's a start.

--- sudoku-client/src/SudokuGame.js
+++ sudoku-client/src/SudokuGame.js
@@ -11,6 +11,33 @@
 import React from 'react';
 import SudokuBoard from './SudokuBoard';
 
+
+class GameTree {
+	constructor() {
+		this.root = null;
+		this.allNodes = [];
+	}
+
+	nodeBySerialNumber(serialNumber) {
+		return this.allNodes[serialNumber];
+	}
+
+	addBoard(newBoard, parent=null) {
+		console.log('addBoard: New board is ' + newBoard + ', type is ' + typeof(newBoard));
+		console.log(newBoard);
+		if (this.root === null || parent === null) {
+			this.root = new TreeNode(null);
+			this.root.board = newBoard;
+		} else {
+			const parentNode = this.nodeBySerialNumber(parent);
+			parentNode.addChildBoard(newBoard);
+		}
+		this.allNodes[newBoard.serial] = newBoard;
+	}
+		
+}
+
+
 class TreeNode {
 	constructor(parent) {
 		this.parent = parent;
@@ -18,20 +45,42 @@ class TreeNode {
 		this.board = null;
 	}
 
-
+	addChildBoard(board) {
+		const childNode = new TreeNode(this);
+		childNode.board = board;
+		this.children.push(childNode);
+	}
 }
 
 
 class SudokuGame extends React.Component {
 	constructor(props) {
 	 	super(props);
-	 	const rootNode = new TreeNode(null);
+
 		this.state = {
-			treeRoot: rootNode
+			gameTree: null
 		}
-	 	if (this.props.initialBoard !== null) {
-	 		this.state.treeRoot.board = this.props.initialBoard;
-	 	}
+		if (this.props.initialBoard !== null) {
+			console.log('SudokuGame: Non-null initial board supplied with props for constructor.');
+			this.state.gameTree = new GameTree();
+			this.state.gameTree.addBoard(this.props.initialBoard);
+		}
+	}
+
+	// When we get our initial board, we need to update/initialize the game tree.
+	componentDidUpdate(oldProps) {
+		console.log('SudokuGame componentDidUpdate called.')
+		if (oldProps.initialBoard !== this.props.initialBoard) {
+			this.initializeGameTree(this.props.initialBoard);
+		}
+	}
+
+	initializeGameTree(rootBoard) {
+		const newGameTree = new GameTree();
+		newGameTree.addBoard(rootBoard);
+		this.setState({
+			gameTree: newGameTree
+		});
 	}
 
 	boardAnnouncesChoice(board, cell, choice) {
@@ -50,33 +99,49 @@ class SudokuGame extends React.Component {
 	}
 
 	render() {
-		return (
-			<div key={3}>
-				<div key={4}>
-				    Hi.  This is the Sudoku game tree.  My responsibilities
-				    are as follows:
-				    <ul>
-				        <li>On startup, go ask some server for an initial board.</li>
-				        <li>Set up, maintain, and render the game tree.</li>
-				        <li>When the user makes a move on a board, contact the
-				            server to get the child nodes resulting from that move.</li>
-				        <li>When the user has finished the game, transmit the entire
-				            game tree to the server along with some metadata yet to
-				            be established.</li>
-				    </ul>
+		console.log('SudokuGame.render(): this.props.initialBoard:');
+		console.log(this.props.initialBoard);
+		if (this.state.gameTree === null) {
+			return (
+				<div>
+				   SudokuGame does not yet have a game tree.  This is OK; it means we don't
+				   have our initial board yet.
 				</div>
-				<div key={5}>
-					<SudokuBoard 
-						degree={this.props.degree}
-						board={this.props.initialBoard} 
-						key={6}
-						announceChoice={ (board, cell, choice) => {this.boardAnnouncesChoice(board,cell,choice);} }
-					/>
-				</div>
-			</div>
 			);
+		} else if (this.state.gameTree.root.board === null) {
+			return (
+				<div>
+				    SudokuGame has a game tree but no root board.  This shouldn't happen.
+				</div>
+			);
+		} else {
+			return (
+				<div key={3}>
+					<div key={4}>
+					    Hi.  This is the Sudoku game tree.  My responsibilities
+					    are as follows:
+					    <ul>
+					        <li>On startup, go ask some server for an initial board.</li>
+					        <li>Set up, maintain, and render the game tree.</li>
+					        <li>When the user makes a move on a board, contact the
+					            server to get the child nodes resulting from that move.</li>
+					        <li>When the user has finished the game, transmit the entire
+					            game tree to the server along with some metadata yet to
+					            be established.</li>
+					    </ul>
+					</div>
+					<div key={5}>
+						<SudokuBoard 
+							degree={this.props.degree}
+							board={this.state.gameTree.root.board} 
+							key={6}
+							announceChoice={ (board, cell, choice) => {this.boardAnnouncesChoice(board,cell,choice);} }
+						/>
+					</div>
+				</div>
+				);
+		}
 	}
-
 }
 
 export default SudokuGame;
